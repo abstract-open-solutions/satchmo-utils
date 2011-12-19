57d432ecc878f7481a7cedd86575b54b98ee770f
Biagio Grimaldi <biagio.grimaldi@abstract.it>
2011-12-19 19:19:47 +0100

    removed suspended view

--- satchmoutils/payments/modules/bonifico/urls.py
+++ satchmoutils/payments/modules/bonifico/urls.py
@@ -7,6 +7,5 @@ config = config_get_group('PAYMENT_BONIFICO')
 
 urlpatterns = patterns('',
     (r'^$', 'satchmoutils.payments.modules.bonifico.views.one_step', {'SSL': ssl}, 'BONIFICO_satchmo_checkout-step2'),
-    (r'^suspended/$', 'satchmoutils.payments.views.suspended', {'SSL': ssl}, 'BONIFICO_satchmo_checkout-suspended'),
     (r'^success/$', 'satchmoutils.payments.modules.views.multisuccess_view', {'SSL': ssl}, 'BONIFICO_satchmo_checkout-success'),    
 )
--- satchmoutils/payments/modules/contrassegno/urls.py
+++ satchmoutils/payments/modules/contrassegno/urls.py
@@ -7,6 +7,5 @@ config = config_get_group('PAYMENT_CONTRASSEGNO')
 
 urlpatterns = patterns('',
     (r'^$', 'satchmoutils.payments.modules.contrassegno.views.one_step', {'SSL': ssl}, 'CONTRASSEGNO_satchmo_checkout-step2'),
-    (r'^suspended/$', 'satchmoutils.payments.views.suspended', {'SSL': ssl}, 'CONTRASSEGNO_satchmo_checkout-suspended'),
     (r'^success/$', 'satchmoutils.payments.modules.views.multisuccess_view', {'SSL': ssl}, 'CONTRASSEGNO_satchmo_checkout-success'),    
 )
