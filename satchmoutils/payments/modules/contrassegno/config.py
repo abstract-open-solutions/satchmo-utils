569ef5b323c869ef16fd753c4fe28dfb2451f957
Biagio Grimaldi <biagio.grimaldi@abstract.it>
2011-12-20 13:30:56 +0100

    added some translations

--- satchmoutils/payments/modules/contrassegno/config.py
+++ satchmoutils/payments/modules/contrassegno/config.py
@@ -33,7 +33,7 @@ config_register_list(
     StringValue(PAYMENT_GROUP,
         'LABEL',
         description=_('English name for this group on the checkout screens'),
-        default = _('Pagamento alla consegna'),
+        default = _('Payment on delivery'),
         help_text = _('This will be passed to the translation utility')),
 
     StringValue(PAYMENT_GROUP,
