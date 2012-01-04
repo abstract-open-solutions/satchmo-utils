c985dc911afd35997e1675c151e5c4155235240e
Simone Deponti <simone.deponti@gmail.com>
2012-01-04 12:52:17 +0100

    Refactored form extension

--- satchmoutils/utils.py
+++ satchmoutils/utils.py
@@ -1,57 +1,70 @@
-import types
-from signals_ahoy.signals import form_init
+from signals_ahoy.signals import form_init, form_initdata, form_postsave
 
-class ExtraHandler(object):
 
-    signal = None
+class ExtraField(object):
+
+    def __init__(self, klass, name, **kwargs):
+        self.name = name
+        self.klass = klass
+        self.kwargs = kwargs
+
+    def __call__(self):
+        return self.klass(**self.kwargs)
+
+
+class Fieldset(object):
+
+    def __init__(self, id_, label, *fields):
+        self.id_ = id_
+        self.label = label
+        self.fields = fields
+        self.form = None
+
+    def bind(self, form):
+        copy = self.__class__(self.id_, self.label, *self.fields)
+        copy.form = form
+        return copy
+
+
+def extends(*form_classes):
+    def wrapper(extender):
+        for form_class in form_classes:
+            form_init.connect(extender.handle_init, sender=form_class)
+            if hasattr(extender, 'handle_initdata'):
+                form_initdata.connect(extender.handle_initdata,
+                                      sender=form_class)
+            if hasattr(extender, 'handle_postsave'):
+                form_postsave.connect(extender.handle_postsave,
+                                      sender=form_class)
+        return extender
+    return wrapper
+
+
+class Extender(object):
+    """Usage
+
+        >>> @extends(MyForm)
+        >>> class MyExtender(Extender):
+        ...     fields = [
+        ...         ExtraField(CharField, name='foo')
+        ...     ]
+        ...     fieldsets = [
+        ...         Fieldset('test', _(u"Test"), 'foo')
+        ...     ]
+        ...     @classmethod
+        ...     def handle_initdata(cls, **kwargs):
+        ...         pass
+        ...     @classmethod
+        ...     def handle_postsave(cls, **kwargs):
+        ...         pass
+    """
 
     @classmethod
-    def hook(cls, form_class):
-        cls.signal.connect(cls(), sender=form_class)
-
-    def __call__(self, **kwargs):
-        pass
-
-
-class metainitializer(type):
-
-    def __call__(cls, *args, **kwargs):
-        dict_ = dict(cls.__dict__.items())
-        if len(args) > 0:
-            dict_['name'] = args[0]
-        if len(args) > 1:
-            dict_['instance'] = args[1]
-        dict_.update(kwargs)
-        return type.__new__(metainitializer, cls.__name__, cls.__bases__, dict_)
-
-
-class ExtraBase(ExtraHandler):
-    __metaclass__ = metainitializer
-    signal = form_init
-
-
-class ExtraField(ExtraBase):
-
-    def __call__(self, **kwargs):
+    def handle_init(cls, **kwargs):
         form = kwargs['form']
-        form.fields[self.name] = self.instance
-
-
-class ExtraMethod(ExtraBase):
-
-    def __call__(self, **kwargs):
-        form = kwargs['form']
-        form_class = kwargs['sender']
-        form.__dict__[self.name] = types.MethodType(
-            self.instance,
-            form,
-            form_class
-        )
-
-
-def add_to_form(form_class, *args):
-    for arg in args:
-        if isinstance(arg, type) and issubclass(arg, ExtraHandler):
-            arg.hook(form_class)
-        else:
-            raise ValueError("Must be an ExtraHandler class")
+        for extrafield in cls.fields:
+            form.fields[extrafield.name] = extrafield()
+        if hasattr(cls, 'fieldsets') and len(cls.fieldsets) > 0:
+            if not hasattr(form, 'fieldsets'):
+                form.fieldsets = []
+            form.fieldsets.extend([ f.bind(form) for f in cls.fieldsets ])
