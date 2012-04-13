60f6307a858cdf310956fff9b7c451cab39c27a9
Biagio Grimaldi <biagio.grimaldi@abstract.it>
2012-04-13 18:47:20 +0200

    added debug value in context-processor

--- satchmoutils/context_processors.py
+++ satchmoutils/context_processors.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from .utils import ShopUtils
 
 
@@ -9,4 +11,5 @@ log = logging.getLogger('primifrutti_shop_context')
 def shop_configs(request):
     shop_utils = ShopUtils()
     ctx = shop_utils.get_shop_config()
+    ctx['debug'] = settings.DEBUG
     return ctx
