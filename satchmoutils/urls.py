25344dd61f4cb7c97a4a6d978a72c85243c611e7
Biagio Grimaldi <biagio.grimaldi@abstract.it>
2012-04-18 13:08:47 +0200

    added customization of satchmo contact-form with captcha field

--- satchmoutils/urls.py
+++ satchmoutils/urls.py
@@ -1,11 +1,14 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url, include
 from satchmo_store.shop import get_satchmo_setting
 
 catbase = r'^' + get_satchmo_setting('CATEGORY_SLUG') + '/'
 prodbase = r'^' + get_satchmo_setting('PRODUCT_SLUG') + '/'
 
 urlpatterns = patterns('',
-    (r'^cart/empty/$', 'satchmoutils.views.cart_empty', {}, 'satchmo_cart_empty'),
-    (r"^test_confirm_ipn/", 'satchmoutils.test_views.test_confirm_ipn', {}, 'test_confirm_ipn'),
-    (r"^captcha/", include('captcha.urls')),
-)
+    url(r'^cart/empty/$',
+        'satchmoutils.views.cart_empty', {}, 'satchmo_cart_empty'),
+    url(r"^test_confirm_ipn/",
+        'satchmoutils.test_views.test_confirm_ipn', {}, 'test_confirm_ipn'),
+    url(r'^contact/$',
+        'satchmoutils.views.contact_form', {}, 'satchmo_contact'),
+    url(r"^captcha/", include('captcha.urls')))
--- satchmoutils/views.py
+++ satchmoutils/views.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
+from django import forms
+from django import http
+from django.core import urlresolvers
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.utils.translation import ugettext_lazy as _
+from satchmo_store.mail import send_store_mail
+from satchmo_store.shop.signals import contact_sender
+from satchmo_store.shop.views.contact import ContactForm as BaseContactForm
+from satchmo_store.shop.models import Cart
+
+from captcha.fields import CaptchaField
+
 import logging
 
-from django.http import HttpResponseRedirect
-from django.core import urlresolvers
-
-from satchmo_store.shop.models import Cart
 
 log = logging.getLogger('primifrutti.ui.views')
 
@@ -15,8 +24,50 @@ def cart_empty(request):
     make empty current cart
     """
     cart = Cart.objects.from_request(request, create=True)
-    
+
     # make cart empty
     cart.empty()
-    return HttpResponseRedirect(urlresolvers.reverse('satchmo_cart'))
-    
+    return http.HttpResponseRedirect(urlresolvers.reverse('satchmo_cart'))
+
+
+# Choices displayed to the user to categorize the type of contact request.
+email_choices = (
+    ("General Question", _("General question")),
+    ("Order Problem", _("Order problem")),
+    ("Product Info", _("Product Info")),
+    ("Shipping Info", _("Shipping Info")),
+)
+
+
+class ContactForm(BaseContactForm):
+    inquiry = forms.ChoiceField(label=_("Inquiry"), choices=email_choices,
+        help_text=_("What about yuor question"))
+    captcha = CaptchaField(required=True,
+        help_text=_("Insert word you see into image"))
+
+
+def contact_form(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            new_data = form.cleaned_data
+            c = {
+                'request_type': new_data['inquiry'],
+                'name': new_data['name'],
+                'email': new_data['sender'],
+                'request_text': new_data['contents']}
+            subject = new_data['subject']
+            send_store_mail(subject, c, 'shop/email/contact_us.txt',
+                            send_to_store=True, sender=contact_sender)
+            url = urlresolvers.reverse('satchmo_contact_thanks')
+            return http.HttpResponseRedirect(url)
+    else: #Not a post so create an empty form
+        initialData = {}
+        if request.user.is_authenticated():
+            initialData['sender'] = request.user.email
+            initialData['name'] = request.user.first_name + " " + \
+                                                    request.user.last_name
+        form = ContactForm(initial=initialData)
+
+    return render_to_response('shop/contact_form.html', {'form': form},
+                              context_instance=RequestContext(request))
